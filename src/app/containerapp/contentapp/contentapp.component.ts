b548795a1c1908c7ae28493531b7d8fa91f69818
wanemar224 <alpha-marouana.diallo9@etu.univ-lorraine.fr>
2021-03-09 17:38:06 +0100

    layout ok

---
+++ src/app/containerapp/contentapp/contentapp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contentapp',
+  templateUrl: './contentapp.component.html',
+  styleUrls: ['./contentapp.component.scss']
+})
+export class ContentappComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/containerapp/pubapp/pubapp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pubapp',
+  templateUrl: './pubapp.component.html',
+  styleUrls: ['./pubapp.component.scss']
+})
+export class PubappComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
